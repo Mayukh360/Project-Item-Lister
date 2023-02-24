527fc01c9442c92c60704e8e10af16dad19d5f79
Mayukh <mayukhchatterjee2023@gmail.com>
2023-02-24 19:15:52 +0530

    Final

--- dom.js
+++ dom.js
@@ -1,13 +1,12 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+
 
 // Add item
 function addItem(e) {
@@ -60,22 +59,8 @@ function removeItem(e) {
   }
 }
 
-// Filter Items
-function filterItems(e) {
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function (item) {
-    var itemName = item.firstChild.textContent;
-    if (itemName.toLowerCase().indexOf(text) != -1) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
+
+
 //Adding search-Bar function
 const searchBar = document.getElementById('filter');
 const list = document.getElementById('items');
